83de2d8cda5bf6cb5c4bce8fca4e005cfd2a8da2
ThienTan <115565563+ttnb2528@users.noreply.github.com>
2024-07-11 19:33:28 +0700

    Add PrivateRoute component for authenticated routes

--- frontend/src/Redux/api/user.api.js
+++ frontend/src/Redux/api/user.api.js
@@ -25,8 +25,20 @@ export const userApiSlice = apiSlice.injectEndpoints({
         body: data,
       }),
     }),
+
+    profile: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/profile`,
+        method: "PUT",
+        body: data,
+      }),
+    }),
   }),
 });
 
-export const { useLoginMutation, useLogoutMutation, useRegisterMutation } =
-  userApiSlice;
+export const {
+  useLoginMutation,
+  useLogoutMutation,
+  useRegisterMutation,
+  useProfileMutation,
+} = userApiSlice;
