44bcb6fb411deede0e78c1f1da705e068cab76ab
ThienTan <115565563+ttnb2528@users.noreply.github.com>
2024-07-18 22:29:51 +0700

    A Deep Dive into Product CRUD Operations

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ import cookieParser from "cookie-parser";
 import connectDB from ".//config/db.js";
 import userRoutes from "./routes/user.route.js";
 import categoryRoutes from "./routes/category.route.js";
-
+import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const port = process.env.PORT || 5000;
@@ -23,5 +23,6 @@ app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
---
+++ backend/routes/product.route.js
@@ -0,0 +1,41 @@
+import express from "express";
+import formidable from "express-formidable";
+const router = express.Router();
+
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
+import checkId from "../middlewares/checkId.js";
+
+// Controller
+import {
+  addProduct,
+  updateProductDetails,
+  removeProduct,
+  getProducts,
+  fetchProductById,
+  fetchAllProducts,
+  addProductReview,
+  fetchTopProducts,
+  fetchNewProducts,
+} from "../controllers/product.controller.js";
+
+router
+  .route("/")
+  .get(getProducts)
+  .post(authenticate, authorizeAdmin, formidable(), addProduct);
+
+router.route("/allProducts").get(fetchAllProducts);
+
+router
+  .route("/:id/reviews")
+  .post(authenticate, authorizeAdmin, checkId, addProductReview);
+
+router.get("/top", fetchTopProducts);
+router.get("/new", fetchNewProducts);
+
+router
+  .route("/:id")
+  .get(fetchProductById)
+  .put(authenticate, authorizeAdmin, formidable(), updateProductDetails)
+  .delete(authenticate, authorizeAdmin, removeProduct);
+
+export default router;
